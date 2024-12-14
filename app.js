ad07fb681f6042cce28ab741db8c9ff595a72c0f
ABDOULLAH EL-HADRAMY <abdoullahhadramy@gmail.com>
2024-12-14 18:22:35 -0500

    create a for loop to append punch in every iteration to the array

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 let info = document.querySelector(".info");
 let turn = "x";
-let array_punch = []
-function win (){
-
-
+let array_punch = [];
+function win() {
+  for (i = 1; i < 9; i++) {
+    array_punch[i] = document.getElementById("punch" + i);
+  }
 }
-
 
 function play(id) {
   let punch_id = document.getElementById(id);
@@ -15,10 +15,9 @@ function play(id) {
     turn = "o";
     info.innerHTML = "IT IS THE TURN OF 'O'";
   } else if (turn == "o" && punch_id.innerHTML == "") {
-  punch_id.innerHTML = "o"
-  turn= "x"
-info.innerHTML = "IT IS THE TURN OF 'X'";
+    punch_id.innerHTML = "o";
+    turn = "x";
+    info.innerHTML = "IT IS THE TURN OF 'X'";
   }
-  win ()
+  win();
 }
-
