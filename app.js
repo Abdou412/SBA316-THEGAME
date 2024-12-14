f871527a57218bf6d8b0e64cbfa2c0849d3987d5
ABDOULLAH EL-HADRAMY <abdoullahhadramy@gmail.com>
2024-12-14 17:46:15 -0500

    add the info div to the DOM and resign the the value of turn

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-let turn = "x"
-function play(id){
-let punch_id = document.getElementById(id)
+let info = document.querySelector(".info");
+let turn = "x";
+function play(id) {
+  let punch_id = document.getElementById(id);
 
-if (turn==="x"&& punch_id.innerHTML==""){
-    punch_id.innerHTML="x"
+  if (turn === "x" && punch_id.innerHTML == "") {
+    punch_id.innerHTML = "x";
+    turn = "o";
+info.innerHTML = "IT IS THE TURN OF 'O'"
+  }
 }
-
-}
-
