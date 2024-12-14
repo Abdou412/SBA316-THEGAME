9aa1fb4c7b1b5f05b17e491d8c174043b94f8205
ABDOULLAH EL-HADRAMY <abdoullahhadramy@gmail.com>
2024-12-14 17:54:19 -0500

    crate the else if statment so i can print the letter o on the board

--- app.js
+++ app.js
@@ -7,5 +7,8 @@ function play(id) {
     punch_id.innerHTML = "x";
     turn = "o";
     info.innerHTML = "IT IS THE TURN OF 'O'";
+  } else if (turn == "o" && punch_id.innerHTML == "") {
+  punch_id.innerHTML = "o"
+
   }
 }
