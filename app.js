a670b4e6256137c3fa3cbf664fee68ff324b93e6
ABDOULLAH EL-HADRAMY <abdoullahhadramy@gmail.com>
2024-12-15 21:51:15 -0500

    add the inputs and css changes...

--- app.js
+++ app.js
@@ -87,18 +87,18 @@ function win() {
     document.getElementById("punch3").style.backgroundColor = "white";
     document.getElementById("punch5").style.backgroundColor = "white";
     document.getElementById("punch7").style.backgroundColor = "white";
-  }
+  } 
 }
 
 function play(id) {
   let punch_id = document.getElementById(id);
 
   if (turn === "x" && punch_id.innerHTML == "") {
-    punch_id.innerHTML = "x";
+    punch_id.innerHTML = "X";
     turn = "o";
     info.innerHTML = "IT IS THE TURN OF 'O'";
   } else if (turn == "o" && punch_id.innerHTML == "") {
-    punch_id.innerHTML = "o";
+    punch_id.innerHTML = "O";
     turn = "x";
     info.innerHTML = "IT IS THE TURN OF 'X'";
   }
